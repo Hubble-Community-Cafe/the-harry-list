09e85f2327ccb00a707bf1fff756616f69db7b7a
Pim van Leeuwen <git@pvleeuwen.com>
2026-05-28 11:40:29 +0200

    fix(vite.config.ts): fix drifting files

--- the-harry-list-admin/vite.config.ts
+++ the-harry-list-admin/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
-import { version } from './package.json'
+import { name, version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +14,7 @@ export default defineConfig({
     tailwindcss(),
     sentryVitePlugin({
       org: 'stichting-bar-potential',
-      project: 'the-harry-list-admin',
+      project: name,
       release: { name: version },
       sourcemaps: { filesToDeleteAfterUpload: ['./dist/**/*.map'] },
       disable: !process.env.SENTRY_AUTH_TOKEN,
