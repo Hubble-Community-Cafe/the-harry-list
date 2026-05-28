063466a4cdc49423ec39da52ab520ad0a8aed5be
Pim van Leeuwen <git@pvleeuwen.com>
2026-05-28 11:25:47 +0200

    ci(sentry): add Sentry release tracking and source maps upload

--- the-harry-list-admin/vite.config.ts
+++ the-harry-list-admin/vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
+      org: 'stichting-bar-potential',
+      project: 'the-harry-list-admin',
+      release: { name: version },
+      sourcemaps: { filesToDeleteAfterUpload: ['./dist/**/*.map'] },
+      disable: !process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
   define: {
     __APP_VERSION__: JSON.stringify(version),
   },
